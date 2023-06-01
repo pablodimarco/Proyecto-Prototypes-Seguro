31b4d1e0f85e3692f2ce7e55c4a4abc098a2fbdb
Pablo Di Marco <pablodimarco7@gmail.com>
2023-06-01 13:00:58 -0300

    Practice

--- js/app.js
+++ js/app.js
@@ -5,6 +5,48 @@ function Seguro(marca, year, tipo) {
     this.tipo = tipo;
 };
 
+// realiza la cotizacion con los datos
+Seguro.prototype.cotizarSeguro = function() {
+    /*
+        1 = americano 1.15
+        2 = asiatico 1.05
+        3 = europeo 1.35 
+    */
+
+    let cantidad;
+    const base = 2000;
+
+    switch(this.marca) {
+        case '1':
+            cantidad = base * 1.15;
+            break;
+        case '2':
+            cantidad = base * 1.05;
+            break;
+        case '3':
+            cantidad = base * 1.35;
+            break;
+        default:
+            break;
+    };
+
+    // leer el año
+    const diferencia = new Date().getFullYear() - this.year;
+
+    // cada año que la diferencia es mayor, el costo va a reducirse un 3%
+    cantidad -= ((diferencia*3) * cantidad) / 100;
+
+    // si el seguro es basico se multiplica por un 30% mas
+    // si el seguro es completo se multiplica por un 50% mas
+    if (this.tipo === 'basico') {
+        cantidad *= 1.30;
+    } else {
+        cantidad *= 1.50;
+    };
+
+    return cantidad;
+};
+
 function UI() {
 
 };
@@ -88,7 +130,8 @@ function cotizarSeguro(e) {
     ui.mostrarMensaje('Cotizando..', 'correcto');
 
     // instanciar el seguro
-
+    const seguro = new Seguro(marca, year, tipo);
+    seguro.cotizarSeguro();
     // utilizar proto que cotiza
 
 };
